a32a5f2f4750813e3f38bd7a1b7b5602a7981b19
carlosbult <acevedo.bult@gmail.com>
2021-05-10 14:42:27 -0400

    Menu Toggle completo

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ let menu_bar = document.querySelector('#menu-bar');
   menu_bar.addEventListener('click', function() {
     menu.classList.toggle("menu__toggle");
   });
+
+  menu.addEventListener('click', function(){
+      menu.classList.toggle("menu__toggle");
+  });
