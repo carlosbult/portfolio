da459fbd205d31c67271899f3ea17e366993ad6e
carlosbult <acevedo.bult@gmail.com>
2021-07-15 13:51:43 -0400

    Easy bank link fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+/* Menu Toogle */
 let menu = document.querySelector('#menu');
 let menu_bar = document.querySelector('#menu-bar');
-    
+
   menu_bar.addEventListener('click', function() {
     menu.classList.toggle("menu__toggle");
   });
@@ -8,3 +9,21 @@ let menu_bar = document.querySelector('#menu-bar');
   menu.addEventListener('click', function(){
       menu.classList.toggle("menu__toggle");
   });
+
+/* Form Validate */
+let contactForm = document.querySelector('#form');
+let inputs = document.querySelectorAll('input[required]');
+
+inputs.forEach(input => {
+  const span = document.createElement('span');
+  span.id = input.name;
+  span.textContent = input.title;
+  span.classList.add('contact-form-error', 'none');
+  input.insertAdjacentElement('afterend', span);
+})
+
+document.addEventListener('keyup',(e) => {
+  if(e.target.matches('input[required]')){
+
+  }
+});
