777cfb530f3e5fa9f10f3347b9b521682081eaa8
carlosbult <acevedo.bult@gmail.com>
2021-07-26 14:36:55 -0400

    Fix

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 /* Menu Toogle */
-let menu = document.querySelector('#menu');
-let menu_bar = document.querySelector('#menu-bar');
+let menu = document.querySelector("#menu");
+let menu_bar = document.querySelector("#menu-bar");
 
-  menu_bar.addEventListener('click', function() {
-    menu.classList.toggle("menu__toggle");
-  });
+menu_bar.addEventListener("click", function () {
+  menu.classList.toggle("menu__toggle");
+});
 
-  menu.addEventListener('click', function(){
-      menu.classList.toggle("menu__toggle");
-  });
+menu.addEventListener("click", function () {
+  menu.classList.toggle("menu__toggle");
+});
 
 /* Form Validate */
-let contactForm = document.querySelector('#form');
-let inputs = document.querySelectorAll('input[required]');
+let contactForm = document.querySelector("#form");
+let inputs = document.querySelectorAll("input[required]");
 
-inputs.forEach(input => {
-  const span = document.createElement('span');
+inputs.forEach((input) => {
+  const span = document.createElement("span");
   span.id = input.name;
   span.textContent = input.title;
-  span.classList.add('contact-form-error', 'none');
-  input.insertAdjacentElement('afterend', span);
-})
+  span.classList.add("contact-form-error", "none");
+  input.insertAdjacentElement("afterend", span);
+});
 
-document.addEventListener('keyup',(e) => {
-  if(e.target.matches('input[required]')){
-
+document.addEventListener("keyup", (e) => {
+  if (e.target.matches("input[required]")) {
   }
 });
