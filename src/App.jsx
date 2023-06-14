0dc7e9d6174e303579843f5fd04e536fba8d4650
carlosbult <acevedo.bult@gmail.com>
2023-06-14 15:10:45 -0400

    Deleted lingui library

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import {
   About,
   Contact,
@@ -6,43 +6,43 @@ import {
   Hero,
   Navbar,
   Tech,
-  Projects,
-} from './components';
+  Projects
+} from './components'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0">
+      <div className='relative z-0'>
         <div>
           <Navbar />
           <Hero />
         </div>
 
-        <div className="bg-about bg-cover bg-center bg-no-repeat">
+        <div className='bg-about bg-cover bg-center bg-no-repeat'>
           <About />
         </div>
 
-        <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
+        <div className='bg-tech bg-cover bg-center bg-no-repeat pb-10'>
           <Tech />
         </div>
 
         <Projects />
 
         <div
-          className="bg-experience bg-cover bg-center bg-no-repeat 
-            rounded-tl-[150px] rounded-br-[150px]">
+          className='bg-experience bg-cover bg-center bg-no-repeat
+            rounded-tl-[150px] rounded-br-[150px]'>
           <div
-            className="bg-experienceLight bg-cover bg-center 
-            bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
+            className='bg-experienceLight bg-cover bg-center
+            bg-no-repeat rounded-tl-[150px] rounded-br-[130px]'>
             <Experience />
           </div>
         </div>
-        <div className="relative z-0">
+        <div className='relative z-0'>
           <Contact />
         </div>
       </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
